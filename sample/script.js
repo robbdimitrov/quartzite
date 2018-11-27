69c0e05ecc7e86c11c4cf497067a68e0de63ce43
Robert Dimitrov <robert_dimitrov@me.com>
2018-11-27 22:36:07 +0200

    Configured webpack dev server, fixed library build, connected example.

--- sample/script.js
+++
@@ -1,6 +0,0 @@
-import { relativeDate } from '../dist/relative-date';
-
-const date = new Date();
-
-document.querySelector('.date').innerHTML = date;
-document.querySelector('.relative-date').innerHTML = relativeDate(date);
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,21 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'relative-date.js',
     library: 'relativeDate',
     libraryTarget: 'umd',
     globalObject: 'this'
+  },
+  devServer: {
+    contentBase: [
+      path.join(__dirname, 'examples'),
+      path.join(__dirname, 'dist')
+    ],
+    compress: true,
+    port: 9000
   },
   module: {
     rules: [
