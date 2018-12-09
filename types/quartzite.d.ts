ece97ac155ee8e3de74d905a236ec44dbf7b83a0
Robert Dimitrov <robert_dimitrov@me.com>
2018-12-09 13:31:35 +0200

    Added ts definitions file.

---
+++ types/quartzite.d.ts
@@ -0,0 +1,7 @@
+export function dateByAdding(type: string, date: Date, value?: number): Date;
+
+export function timeDifference(now: Date, date: Date, style?: string): string;
+
+export function dateDifference(ow: Date, date: Date, style?: string): string;
+
+export function formatDate(date: Date, style?: string): string;
