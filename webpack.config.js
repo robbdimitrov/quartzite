834c366a8da61cb5213709562eb25c9844753bf9
Robert Dimitrov <robert_dimitrov@me.com>
2018-12-09 23:01:15 +0200

    Removed sourcemap option from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'quartzite.js',
