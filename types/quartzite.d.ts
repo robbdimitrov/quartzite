ec61595729da9e3386d13a77935b1d1ab4038dee
Robert Dimitrov <robert_dimitrov@me.com>
2018-12-09 22:57:57 +0200

    Updated typings file, added unpkg field to package.json

--- types/quartzite.d.ts
+++ types/quartzite.d.ts
@@ -1,7 +1,11 @@
+type StyleKey = 'normal' | 'short';
+
+export as namespace quartzite;
+
 export function dateByAdding(type: string, date: Date, value?: number): Date;
 
-export function timeDifference(now: Date, date: Date, style?: string): string;
+export function timeDifference(now: Date, date: Date, style?: StyleKey): string;
 
-export function dateDifference(ow: Date, date: Date, style?: string): string;
+export function dateDifference(ow: Date, date: Date, style?: StyleKey): string;
 
-export function formatDate(date: Date, style?: string): string;
+export function formatDate(date: Date, style?: StyleKey): string;
