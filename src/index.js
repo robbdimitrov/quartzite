5f100f48ed536de0cf8fa7531e7143cc3b2403cc
Robert Dimitrov <robert_dimitrov@me.com>
2018-12-26 20:40:37 +0200

    Added doc comments

--- src/index.js
+++ src/index.js
@@ -14,10 +14,15 @@ const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
 
 // Modifiers
 
-//
-// type can be: 'days', 'hours', 'minutes', 'seconds'
-//
-
+/**
+ * Returns a date, which is the passed date with added time value
+ * @param {string} type - The type of value to add to the passed date.
+ *  Allowed options are 'days', 'hours', 'minutes' and 'seconds'.
+ *  'seconds' is the default.
+ * @param {Date} date - The reference date used to construct the resulting date
+ * @param {number} value - The number of days/hours/seconds to add to the date
+ * @returns {Date} A new date
+ */
 export function dateByAdding(type, date, value) {
     let nextDate = new Date(date);
 
@@ -40,6 +45,9 @@ export function dateByAdding(type, date, value) {
 
 // Formatters
 
+/**
+ * Returns date string with month and day
+ */
 function monthDateFormat(date, style) {
     if (style === 'short') {
         return `${monthsShort[date.getMonth()]} ${date.getDate()}`;
@@ -47,6 +55,9 @@ function monthDateFormat(date, style) {
     return `${months[date.getMonth()]} ${date.getDate()}`;
 }
 
+/**
+ * Returns date string with year
+ */
 function yearDateFormat(date, style) {
     if (style === 'short') {
         return `${date.getDate()} ${monthsShort[date.getMonth()]} ${date.getFullYear()}`;
@@ -54,16 +65,29 @@ function yearDateFormat(date, style) {
     return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 }
 
+/**
+ * Returns time string with format hh:mm
+ */
 function timeFormat(date) {
     return `${numberFormat(date.getHours())}:${numberFormat(date.getMinutes())}`;
 }
 
+/**
+ * Prepends zero in front of a number if the number is less than 10
+ */
 function numberFormat(number) {
     return `${number < 10 ? '0' : ''}${number}`;
 }
 
 // Helpers
 
+/**
+ * Calculates the time difference between two dates
+ * @param {Date} date1 - The first date
+ * @param {Date} date2 - The second date
+ * @returns {object} Object containing the difference in days,
+ *  hours, minutes and seconds
+ */
 function difference(date1, date2) {
     let seconds = Math.round(Math.abs(date1 - date2) / 1000);
     let minutes = Math.round(seconds / 60);
@@ -71,26 +95,48 @@ function difference(date1, date2) {
     let days = Math.round(hours / 24);
 
     return {
-        seconds,
-        minutes,
+        days,
         hours,
-        days
+        minutes,
+        seconds
     };
 }
 
+/**
+ * Compares two dates to determines if they belong to the same week
+ * @param {Date} date1 - The first date
+ * @param {Date} date2 - The second date
+ * @returns {boolean}
+ */
 function isSameWeek(date1, date2) {
     let weekStart = dateByAdding('days', date1, -7);
     return (date2 >= weekStart && date2 <= date1);
 }
 
+/**
+ * Returns singular or plural version of a string
+ * @param {number} number - The number used to determine which
+ *  version of the string to return
+ * @param {string} singular - The singular string
+ * @param {string} plural - The plural string
+ * @returns {string}
+ */
 function pluralize(number, singular, plural) {
     return `${number} ${number === 1 ? singular : plural}`;
 }
 
-// Time difference
+// Relativity
 
+/**
+ * Formats a date by making a time comparison
+ * @param {Date} now - The relative date used for comprison
+ * @param {Date} date - The date to format
+ * @param {string} style - The style of the formatter
+ *  Allowed options are 'normal' and 'short'. 'normal' is the default.
+ * @returns {string}
+ */
 export function timeDifference(now, date, style = 'normal') {
-    let { seconds, minutes, hours, days } = difference(now, date);
+    let { days, hours, minutes, seconds } = difference(now, date);
 
     if (style === 'normal' && seconds < 5) {
         return 'Just now';
@@ -123,6 +169,14 @@ export function timeDifference(now, date, style = 'normal') {
     return `${now < date ? 'In ' : ''}${result}${now > date ? ' ago' : ''}`;
 }
 
+/**
+ * Formats a date by making a day comparison
+ * @param {Date} now - The relative date used for comprison
+ * @param {Date} date - The date to format
+ * @param {string} style - The style of the formatter
+ *  Allowed options are 'normal' and 'short'. 'normal' is the default.
+ * @returns {string}
+ */
 export function dateDifference(now, date, style = 'normal') {
     let isCurrent = now.getFullYear() === date.getFullYear()
         && now.getMonth() === date.getMonth();
@@ -144,12 +198,13 @@ export function dateDifference(now, date, style = 'normal') {
     return yearDateFormat(date, style);
 }
 
-// Relativity
-
-//
-// style options: 'normal', 'short'
-//
-
+/**
+ * Formats a date by comparing with the current date
+ * @param {Date} date - The date to format
+ * @param {string} style - The style of the formatter
+ *  Allowed options are 'normal' and 'short'. 'normal' is the default.
+ * @returns {string}
+ */
 export function formatDate(date, style = 'normal') {
     let now = new Date();
     let { days } = difference(now, date);
