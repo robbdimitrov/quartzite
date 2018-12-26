1636146194ec2f07d7741044b503ece63d5c3f04
Robert Dimitrov <robert_dimitrov@me.com>
2018-12-26 19:52:52 +0200

    Added tests

--- test/tests.js
+++ test/tests.js
@@ -5,16 +5,24 @@ const quartzite = require('../dist/quartzite');
 
 describe('Quartzite', function() {
     describe('#formatDate()', function() {
-        it('should return "Just now" for dates within 5 seconds', function() {
+        it('should return "Just now"', function() {
             assert.equal(quartzite.formatDate(new Date()), 'Just now');
         });
-        it('should contain "Yesterday"', function() {
+        it('should contain "Yesterday at"', function() {
             let date = quartzite.dateByAdding('hours', new Date(), -25);
-            assert.equal(quartzite.formatDate(date).includes('Yesterday'), true);
+            assert.equal(quartzite.formatDate(date).includes('Yesterday at'), true);
         });
-        it('should contain "Tomorrow"', function() {
+        it('should contain "Tomorrow at"', function() {
             let date = quartzite.dateByAdding('hours', new Date(), 25);
-            assert.equal(quartzite.formatDate(date).includes('Tomorrow'), true);
+            assert.equal(quartzite.formatDate(date).includes('Tomorrow at'), true);
+        });
+        it('should return "30 seconds ago"', function() {
+            let date = quartzite.dateByAdding('seconds', new Date(), -30);
+            assert.equal(quartzite.formatDate(date), '30 seconds ago');
+        });
+        it('should return "In 25 minutes"', function() {
+            let date = quartzite.dateByAdding('minutes', new Date(), 25);
+            assert.equal(quartzite.formatDate(date), 'In 25 minutes');
         });
     });
 });
